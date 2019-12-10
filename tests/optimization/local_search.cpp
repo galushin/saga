7e7437fb34cf3ba51bc7a0b67612ac006cd278ef
galushin <galushin@gmail.com>
2019-12-10 22:14:37 +0700

    Новый тест для локального поиска для псевдо-булевой оптимизации #6
    (переход по первому улучшению, на каждом шаге меняем значение одной переменной)
    
    Целевая функция: расстояние до заданного вектора (выбирается случайно на каждой попытке), а не до нулевого вектора

--- tests/optimization/local_search.cpp
+++ tests/optimization/local_search.cpp
@@ -5,9 +5,25 @@
 #include <saga/random/iid_distribution.hpp>
 #include "../random_engine.hpp"
 
-TEST_CASE("local search (pseudoboolen, first improvement)")
+template <class Argument, class Objective, class RealType>
+void test_local_search_boolean(Objective const & objective, typename Argument::size_type dim,
+                               RealType desired_value, RealType eps = 0)
 {
-    auto const dim = 20;
+    // @todo Через тестирование, основанное на свойствах
+    saga::iid_distribution<std::bernoulli_distribution> init_distr(dim);
+
+    for(auto N = 100; N > 0; -- N)
+    {
+        auto const x_init = init_distr(saga_test::random_engine());
+        auto const x_result = saga::local_search_boolean(objective, x_init);
+
+        CAPTURE(x_result);
+        REQUIRE(objective(x_result) == Approx(desired_value).epsilon(eps));
+    }
+}
+
+TEST_CASE("local search (pseudoboolen, first improvement) : L1 norm")
+{
 
     using Argument = std::vector<bool>;
     auto const objective = [](Argument const & arg)
@@ -15,15 +31,27 @@ TEST_CASE("local search (pseudoboolen, first improvement)")
         return std::count(arg.begin(), arg.end(), true);
     };
 
+    // @todo Тестировать для разных размерностей?
+    auto const dim = 20;
+    test_local_search_boolean<Argument>(objective, dim, dim);
+}
+
+TEST_CASE("local search (pseudoboolen, first improvement) : L1 distance to some vector")
+{
+    using Argument = std::vector<bool>;
+
+    auto const dim = 20;
     saga::iid_distribution<std::bernoulli_distribution> init_distr(dim);
 
-    // @todo Через тестирование, основанное на свойствах
-    for(auto N = 100; N > 0; -- N)
+    auto const x_opt = init_distr(saga_test::random_engine());
+    auto const objective = [&x_opt](Argument const & arg)
     {
-        auto const x_init = init_distr(saga_test::random_engine());
-        auto const x_result = saga::local_search_boolean(objective, x_init);
+        assert(x_opt.size() == arg.size());
 
-        CAPTURE(x_result);
-        REQUIRE(objective(x_result) == dim);
-    }
+        return std::inner_product(arg.begin(), arg.end(), x_opt.begin(), 0*arg.size(),
+                                  std::plus<>{}, std::not_equal_to<>{});
+    };
+
+    // @todo Тестировать для разных размерностей?
+    test_local_search_boolean<Argument>(objective, dim, dim);
 }
