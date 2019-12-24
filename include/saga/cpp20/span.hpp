6fe9c2990e28ddaa4a67d33702494f020bb5fb5a
galushin <galushin@gmail.com>
2019-12-24 12:28:16 +0700

    span: типы и конструктор без параметров

--- include/saga/cpp20/span.hpp
+++ include/saga/cpp20/span.hpp
@@ -4,15 +4,70 @@
 /** @file saga/cpp20/span.hpp
  @brief Реализация std::span из C++20, описанный в:
   www.open-std.org/jtc1/sc22/wg21/docs/papers/2018/n4741.pdf
-  www.open-std.org/jtc1/sc22/wg21/docs/papers/2018/p1024r0.pdf
+  www.open-std.org/jtc1/sc22/wg21/docs/papers/2019/p1024r2.pdf
 */
 
 #include <cstddef>
+#include <iterator>
+#include <type_traits>
 
 namespace saga
 {
-    template <class ElementType, std::ptrdiff_t Extent>
-    class span;
+    // @todo Должно быть inline
+    constexpr std::ptrdiff_t dynamic_extent = -1;
+
+    namespace detail
+    {
+        template <bool Enable>
+        struct default_ctor_enabler
+        {};
+
+        template <>
+        struct default_ctor_enabler<false>
+        {
+            default_ctor_enabler() = delete;
+        };
+    }
+    // namespace details
+
+    template <class ElementType, std::ptrdiff_t Extent = dynamic_extent>
+    class span
+     : detail::default_ctor_enabler<(Extent <= 0)>
+    {
+    public:
+        // Константы и типы
+        using element_type = ElementType;
+        using value_type = std::remove_cv_t<element_type>;
+        using index_type = std::ptrdiff_t;
+        using difference_type = std::ptrdiff_t;
+        using pointer = element_type *;
+        using reference = element_type &;
+        using iterator = element_type *;
+        using const_iterator = element_type const *;
+        using reverse_iterator = std::reverse_iterator<iterator>;
+        using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+        static constexpr index_type extent = Extent;
+
+        // Конструкторы, копирование и присваивание
+        constexpr span() noexcept = default;
+
+        // Подинтервалы
+        // Свойства
+        constexpr index_type size() const noexcept
+        {
+            return this->size_;
+        }
+
+        // Доступ к элементам
+        constexpr pointer data() const noexcept
+        {
+            return this->data_;
+        }
+
+    private:
+        pointer data_ = nullptr;
+        index_type size_ = 0;
+    };
 }
 // namespace saga
 
--- tests/cpp20/span.cpp
+++ tests/cpp20/span.cpp
@@ -2,6 +2,8 @@
 
 #include <catch/catch.hpp>
 
+// @todo Убедиться, что итераторы являются непрерывными
+
 TEST_CASE("span: types and static constants, static extent")
 {
     using Value = double;
@@ -15,12 +17,71 @@ TEST_CASE("span: types and static constants, static extent")
     static_assert(std::is_same<Span::difference_type, ptrdiff_t>::value, "");
     static_assert(std::is_same<Span::pointer, Element*>::value, "");
     static_assert(std::is_same<Span::reference, Element&>::value, "");
-    static_assert(std::is_same<Span::iterator, Element&>::value, "");
-    static_assert(std::is_same<Span::const_iterator, Element&>::value, "");
-    static_assert(std::is_same<Span::reverse_iterator, Element&>::value, "");
-    static_assert(std::is_same<Span::const_reverse_iterator, Element&>::value, "");
+
+    using Iter = Span::iterator;
+    using CIter = Span::const_iterator;
+    static_assert(std::is_same<std::iterator_traits<Iter>::iterator_category,
+                               std::random_access_iterator_tag>::value, "");
+    static_assert(std::is_same<std::iterator_traits<CIter>::iterator_category,
+                               std::random_access_iterator_tag>::value, "");
+    static_assert(std::is_same<Span::reverse_iterator,
+                               std::reverse_iterator<Iter>>::value, "");
+    static_assert(std::is_same<Span::const_reverse_iterator,
+                               std::reverse_iterator<CIter>>::value, "");
     static_assert(Span::extent == n_elements, "");
-    static_assert(std::is_same<decltype(Span::extent), index_type>::value, "");
+    static_assert(std::is_same<decltype(Span::extent),
+                               Span::index_type const>::value, "");
 }
 
-// @todo �������� ����� � ����������� �������� ��� dynamic_extent
+TEST_CASE("span: types and static constants, dynamic extent")
+{
+    using Value = double;
+    using Element = Value const;
+    using Span_default = saga::span<Element>;
+    using Span = saga::span<Element, saga::dynamic_extent>;
+
+    static_assert(std::is_same<Span, Span_default>::value, "");
+
+    static_assert(std::is_same<Span::element_type, Element>::value, "");
+    static_assert(std::is_same<Span::value_type, Value>::value, "");
+    static_assert(std::is_same<Span::index_type, ptrdiff_t>::value, "");
+    static_assert(std::is_same<Span::difference_type, ptrdiff_t>::value, "");
+    static_assert(std::is_same<Span::pointer, Element*>::value, "");
+    static_assert(std::is_same<Span::reference, Element&>::value, "");
+
+    using Iter = Span::iterator;
+    using CIter = Span::const_iterator;
+    static_assert(std::is_same<std::iterator_traits<Iter>::iterator_category,
+                               std::random_access_iterator_tag>::value, "");
+    static_assert(std::is_same<std::iterator_traits<CIter>::iterator_category,
+                               std::random_access_iterator_tag>::value, "");
+    static_assert(std::is_same<Span::reverse_iterator,
+                               std::reverse_iterator<Iter>>::value, "");
+    static_assert(std::is_same<Span::const_reverse_iterator,
+                               std::reverse_iterator<CIter>>::value, "");
+    static_assert(Span::extent == saga::dynamic_extent, "");
+    static_assert(std::is_same<decltype(Span::extent),
+                               Span::index_type const>::value, "");
+}
+
+TEST_CASE("span : default constructor")
+{
+    using Element = int;
+
+    // Динамическая размерность
+    const constexpr saga::span<Element> obj{};
+
+    static_assert(obj.size() == 0, "");
+    static_assert(obj.data() == nullptr, "");
+    static_assert(noexcept(saga::span<Element>{}), "");
+
+    // Нулевая размерность
+    const constexpr saga::span<Element, 0> obj_0{};
+
+    static_assert(obj_0.size() == 0, "");
+    static_assert(obj_0.data() == nullptr, "");
+    static_assert(noexcept(saga::span<Element, 0>{}), "");
+
+    // Статическая положительная размерность
+    static_assert(!std::is_default_constructible<saga::span<Element, 7>>::value, "");
+}
