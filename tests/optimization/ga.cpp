bfa588954f3c3b5554bcb2fc897d16191adc2d20
galushin <galushin@gmail.com>
2020-03-17 18:28:36 +0700

    ГА ПБО: выбор между минимизацией и максимизацией
    Close #73

--- tests/optimization/ga.cpp
+++ tests/optimization/ga.cpp
@@ -52,7 +52,7 @@ TEST_CASE("GA pseudoboolean : initializing population")
 namespace
 {
     template <class Crossover>
-    void test_ga_boolean()
+    void test_ga_boolean_manhattan_distance_min()
     {
         auto const dim = 20;
         using Argument = std::valarray<bool>;
@@ -90,14 +90,59 @@ namespace
 
         REQUIRE(saga::equal(saga::cursor::all(population[best]), saga::cursor::all(x_opt)));
     }
+
+    template <class Crossover>
+    void test_ga_boolean_manhattan_distance_max()
+    {
+        auto const dim = 20;
+        using Argument = std::valarray<bool>;
+
+        saga::iid_distribution<std::bernoulli_distribution, Argument> init_distr(dim);
+
+        auto const x_opt = init_distr(saga_test::random_engine());
+        auto const objective = [&x_opt](Argument const & arg)
+        {
+            return arg.size() - saga::boolean_manhattan_distance(arg, x_opt);
+        };
+
+        auto const problem
+            = saga::make_optimization_problem_boolean(objective, dim, std::greater<>{});
+
+        using Genotype = std::valarray<bool>;
+        saga::GA_settings<Genotype, Crossover> settings;
+        settings.population_size = 200;
+        settings.max_iterations = 200;
+
+        auto const population
+            = saga::genetic_algorithm(problem, settings, saga_test::random_engine());
+
+        std::vector<double> obj_values;
+        obj_values.reserve(settings.population_size);
+
+        std::transform(saga::begin(population), saga::end(population),
+                       std::back_inserter(obj_values), objective);
+
+        auto const best = std::max_element(obj_values.begin(), obj_values.end())
+                        - obj_values.begin();
+
+        CAPTURE(x_opt);
+        CAPTURE(population[best]);
+        CAPTURE(obj_values[best]);
+
+        REQUIRE(saga::equal(saga::cursor::all(population[best]), saga::cursor::all(x_opt)));
+    }
 }
 
 TEST_CASE("GA boolean : minimize manhattan distance")
 {
-    for(auto T = 50; T > 0; -- T)
+    for(auto T = 10; T > 0; -- T)
     {
-        ::test_ga_boolean<saga::ga_boolean_crossover_uniform_fn>();
-        ::test_ga_boolean<saga::ga_boolean_crossover_one_point_fn>();
-        ::test_ga_boolean<saga::ga_boolean_crossover_two_point_fn>();
+        ::test_ga_boolean_manhattan_distance_min<saga::ga_boolean_crossover_uniform_fn>();
+        ::test_ga_boolean_manhattan_distance_min<saga::ga_boolean_crossover_one_point_fn>();
+        ::test_ga_boolean_manhattan_distance_min<saga::ga_boolean_crossover_two_point_fn>();
+
+        ::test_ga_boolean_manhattan_distance_max<saga::ga_boolean_crossover_uniform_fn>();
+        ::test_ga_boolean_manhattan_distance_max<saga::ga_boolean_crossover_one_point_fn>();
+        ::test_ga_boolean_manhattan_distance_max<saga::ga_boolean_crossover_two_point_fn>();
     }
 }
