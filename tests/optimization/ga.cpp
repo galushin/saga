00edc362b8f45bb2238248d628d610285fc8d8ed
galushin <galushin@gmail.com>
2020-03-24 20:00:12 +0700

    ГА: пропорциональная селекция
    Вернули элитарность, без неё пропорциональная останавливается
    в одном шаге от оптимума
    
    Close #81

--- tests/optimization/ga.cpp
+++ tests/optimization/ga.cpp
@@ -177,3 +177,16 @@ TEST_CASE("GA boolean : manhattan distance, ranking selection")
     ::test_ga_boolean_manhattan_distance_max<saga::ga_boolean_crossover_one_point_fn>(selection);
     ::test_ga_boolean_manhattan_distance_max<saga::ga_boolean_crossover_two_point_fn>(selection);
 }
+
+TEST_CASE("GA boolean : manhattan distance, proportional selection")
+{
+    saga::selection_proportional selection;
+
+    ::test_ga_boolean_manhattan_distance_min<saga::ga_boolean_crossover_uniform_fn>(selection);
+    ::test_ga_boolean_manhattan_distance_min<saga::ga_boolean_crossover_one_point_fn>(selection);
+    ::test_ga_boolean_manhattan_distance_min<saga::ga_boolean_crossover_two_point_fn>(selection);
+
+    ::test_ga_boolean_manhattan_distance_max<saga::ga_boolean_crossover_uniform_fn>(selection);
+    ::test_ga_boolean_manhattan_distance_max<saga::ga_boolean_crossover_one_point_fn>(selection);
+    ::test_ga_boolean_manhattan_distance_max<saga::ga_boolean_crossover_two_point_fn>(selection);
+}
