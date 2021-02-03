82bf3df348806a68d620115e066dacd69743cade
galushin <galushin@gmail.com>
2021-02-03 22:12:12 +0700

    Пример программы "Локальный поиск" - поддержка целочисленных
    Close #290

--- example/local_search/main.cpp
+++ example/local_search/main.cpp
@@ -42,6 +42,21 @@ public:
         }
     }
 
+    std::function<IntegerSignature> get_integer(Name const & name) const
+    {
+        // @todo Устранить дублирование
+        auto ptr = this->integer_functions_.find(name);
+
+        if(ptr != integer_functions_.end())
+        {
+            return {ptr->second};
+        }
+        else
+        {
+            return {};
+        }
+    }
+
 protected:
     void do_add(BooleanSignature * fun, Name const & name) override
     {
@@ -64,7 +79,7 @@ void print_optimization_state(OStream & out, InputRange const & solution, RealTy
     using Value = typename std::iterator_traits<decltype(std::begin(solution))>::value_type;
     using OutType = std::conditional_t<std::is_same<Value, bool>{}, int, Value>;
 
-    std::copy(std::begin(solution), std::end(solution), std::ostream_iterator<OutType>(out));
+    std::copy(std::begin(solution), std::end(solution), std::ostream_iterator<OutType>(out, " "));
     out << "\t" << objective_value << "\n";
 }
 
@@ -105,6 +120,20 @@ int main(int argc, char * argv[])
         return 1;
     }
 
+    // @todo Считать свойства целочисленных переменных
+    auto maybe_ints_subtree = problem_tree.get_child_optional("problem.integer_variables");
+
+    saga::search_space_integer<int> int_space;
+
+    if(!!maybe_ints_subtree)
+    {
+        for(auto const & node : *maybe_ints_subtree)
+        {
+            assert(node.first == "variable");
+            int_space.add(node.second.get<int>("min"), node.second.get<int>("max"));
+        }
+    }
+
     // Реальная работа
     my_function_manager manager;
 
@@ -113,7 +142,10 @@ int main(int argc, char * argv[])
 
     plugin->init(manager);
 
-    if(bool_dim > 0)
+    std::seed_seq seed{std::time(nullptr)};
+    std::mt19937 random_engine(seed);
+
+    if(bool_dim > 0 && int_space.dim() == 0)
     {
     // @todo Вынести в отдельную функцию
         auto objective_impl = manager.get_boolean(objective_name);
@@ -131,21 +163,53 @@ int main(int argc, char * argv[])
             return objective_impl(s);
         };
 
-        std::seed_seq seed{std::time(nullptr)};
-        std::mt19937 random_engine(seed);
-
         saga::iid_distribution<std::bernoulli_distribution, std::valarray<bool>> init_distr(bool_dim);
         auto const x_init = init_distr(random_engine);
 
         print_optimization_state(std::cout, x_init, objective(x_init));
 
+        // @todo type-erase функцию сравнения
         auto const result =
             (goal == "min") ? saga::local_search_boolean(objective, x_init)
                             : saga::local_search_boolean(objective, x_init, std::greater<>{});
 
         print_optimization_state(std::cout, result.solution, result.objective_value);
     }
-    // @todo Обрабатывать функции с целочисленными переменными
+    else if(bool_dim == 0 && int_space.dim() > 0)
+    {
+        // @todo Вынести в отдельную функцию
+        auto objective_impl = manager.get_integer(objective_name);
+
+        if(!objective_impl)
+        {
+            std::cout << "Unknown objective name: " << objective_name << "\n";
+            return 1;
+        }
+
+        using Vector = std::vector<int>;
+        auto objective = [&](Vector const & arg)
+        {
+            saga::integer_const_span s(arg.size() == 0 ? nullptr : std::addressof(arg[0]), arg.size());
+
+            return objective_impl(s);
+        };
+
+        Vector x_init;
+        for(auto const & var : int_space)
+        {
+            std::uniform_int_distribution<Vector::value_type> distr(var.min, var.max);
+            x_init.push_back(distr(random_engine));
+        }
+
+        print_optimization_state(std::cout, x_init, objective(x_init));
+
+        // @todo type-erase функцию сравнения
+        auto const result =
+            (goal == "min") ? saga::local_search_integer(int_space, objective, x_init)
+                            : saga::local_search_integer(int_space, objective, x_init, std::greater<>{});
+
+        print_optimization_state(std::cout, result.solution, result.objective_value);
+    }
     else
     {
         std::cout << "Unknown problem type\n";
