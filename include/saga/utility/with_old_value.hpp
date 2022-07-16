41b3ce2606a5d5decb140dc2867fabba41580850
galushin <galushin@gmail.com>
2022-07-16 13:00:29 +0700

    Обёртка со старым значением
    тесты основной функциональности
    
    #1027

---
+++ include/saga/utility/with_old_value.hpp
@@ -0,0 +1,90 @@
+/* (c) 2022 Галушин Павел Викторович, galushin@gmail.com
+
+Данный файл -- часть библиотеки SAGA.
+
+SAGA -- это свободной программное обеспечение: вы можете перераспространять ее и/или изменять ее
+на условиях Стандартной общественной лицензии GNU в том виде, в каком она была опубликована Фондом
+свободного программного обеспечения; либо версии 3 лицензии, либо (по вашему выбору) любой более
+поздней версии.
+
+Это программное обеспечение распространяется в надежде, что оно будет полезной, но БЕЗО ВСЯКИХ
+ГАРАНТИЙ; даже без неявной гарантии ТОВАРНОГО ВИДА или ПРИГОДНОСТИ ДЛЯ ОПРЕДЕЛЕННЫХ ЦЕЛЕЙ.
+Подробнее см. в Стандартной общественной лицензии GNU.
+
+Вы должны были получить копию Стандартной общественной лицензии GNU вместе с этим программным
+обеспечение. Если это не так, см. https://www.gnu.org/licenses/.
+*/
+
+#ifndef Z_SAGA_UTILITY_WITH_OLD_VALUE_HPP_INCLUDED
+#define Z_SAGA_UTILITY_WITH_OLD_VALUE_HPP_INCLUDED
+
+/** @file saga/utility/with_old_value.hpp
+ @brief Обёртка для значения, которая также хранит "старое" значение и позволяет к нему вернуться.
+*/
+
+#include <saga/utility/operators.hpp>
+
+#include <utility>
+#include <tuple>
+
+namespace saga
+{
+    template <class T>
+    class with_old_value
+     : saga::operators::equality_comparable<with_old_value<T>>
+    {
+        friend bool operator==(with_old_value const & lhs, with_old_value const & rhs)
+        {
+            return std::tie(lhs.value(), lhs.old_value())
+                    == std::tie(rhs.value(), rhs.old_value());
+        }
+
+    public:
+        // Создание, копирование, уничтожение
+        explicit with_old_value(T value)
+         : value_(std::move(value))
+         , old_value_(this->value_)
+        {}
+
+        with_old_value & operator=(T value)
+        {
+            this->value_ = std::move(value);
+
+            return *this;
+        }
+
+        // Немодифицирующие операции
+        T & value()
+        {
+            return this->value_;
+        }
+
+        T const & value() const
+        {
+            return this->value_;
+        }
+
+        T const & old_value() const
+        {
+            return this->old_value_;
+        }
+
+        // Принятие/сброс изменений
+        void commit()
+        {
+            this->old_value_ = this->value_;
+        }
+
+        void revert()
+        {
+            this->value_ = this->old_value_;
+        }
+
+    private:
+        T value_;
+        T old_value_;
+    };
+}
+
+#endif
+// Z_SAGA_UTILITY_WITH_OLD_VALUE_HPP_INCLUDED
---
+++ tests/utility/with_old_value.cpp
@@ -0,0 +1,140 @@
+/* (c) 2022 Галушин Павел Викторович, galushin@gmail.com
+
+Данный файл -- часть библиотеки SAGA.
+
+SAGA -- это свободной программное обеспечение: вы можете перераспространять ее и/или изменять ее
+на условиях Стандартной общественной лицензии GNU в том виде, в каком она была опубликована Фондом
+свободного программного обеспечения; либо версии 3 лицензии, либо (по вашему выбору) любой более
+поздней версии.
+
+Это программное обеспечение распространяется в надежде, что оно будет полезной, но БЕЗО ВСЯКИХ
+ГАРАНТИЙ; даже без неявной гарантии ТОВАРНОГО ВИДА или ПРИГОДНОСТИ ДЛЯ ОПРЕДЕЛЕННЫХ ЦЕЛЕЙ.
+Подробнее см. в Стандартной общественной лицензии GNU.
+
+Вы должны были получить копию Стандартной общественной лицензии GNU вместе с этим программным
+обеспечение. Если это не так, см. https://www.gnu.org/licenses/.
+*/
+
+// Тестируемый файл
+#include <saga/utility/with_old_value.hpp>
+
+// Тестовая инфраструктура
+#include <catch/catch.hpp>
+#include "../saga_test.hpp"
+
+// Тесты
+TEST_CASE("with_old_value: initialization")
+{
+    using Value = std::string;
+
+    saga_test::property_checker << [](Value const & value)
+    {
+        saga::with_old_value<Value> const obj(value);
+
+        REQUIRE(obj.value() == value);
+        REQUIRE(obj.old_value() == value);
+    };
+}
+
+TEST_CASE("with_old_value: value assignment")
+{
+    using Value = std::string;
+
+    saga_test::property_checker << [](Value const & value, Value const & new_value)
+    {
+        saga::with_old_value<Value> obj(value);
+
+        REQUIRE(obj.value() == value);
+        REQUIRE(obj.old_value() == value);
+
+        obj = new_value;
+
+        REQUIRE(obj.value() == new_value);
+        REQUIRE(obj.old_value() == value);
+    };
+}
+
+TEST_CASE("with_old_value: equality")
+{
+    using Value = std::string;
+
+    saga_test::property_checker << [](Value const & value1, Value const & new_value1
+                                      , Value const & value2, Value const & new_value2)
+    {
+        saga::with_old_value<Value> obj1(value1);
+        saga::with_old_value<Value> obj2(value2);
+
+        obj1 = new_value1;
+        obj2 = new_value2;
+
+        REQUIRE(obj1 == obj1);
+        REQUIRE(obj2 == obj2);
+
+        REQUIRE((obj1 == obj2) == (value1 == value2 && new_value1 == new_value2));
+        REQUIRE((obj1 != obj2) == !(obj1 == obj2));
+    };
+}
+
+TEST_CASE("with_old_value: modifying access")
+{
+    using Value = int;
+
+    saga_test::property_checker << [](Value const & value)
+    {
+        saga::with_old_value<Value> obj(value);
+
+        REQUIRE(obj.value() == value);
+        REQUIRE(obj.old_value() == value);
+
+        ++obj.value();
+
+        REQUIRE(obj.value() == value + 1);
+        REQUIRE(obj.old_value() == value);
+    };
+}
+
+TEST_CASE("with_old_value: commit")
+{
+    using Value = std::string;
+
+    saga_test::property_checker << [](Value const & value, Value const & new_value)
+    {
+        saga::with_old_value<Value> obj(value);
+
+        REQUIRE(obj.value() == value);
+        REQUIRE(obj.old_value() == value);
+
+        obj = new_value;
+
+        REQUIRE(obj.value() == new_value);
+        REQUIRE(obj.old_value() == value);
+
+        obj.commit();
+
+        REQUIRE(obj.value() == new_value);
+        REQUIRE(obj.old_value() == new_value);
+    };
+}
+
+TEST_CASE("with_old_value: revert")
+{
+    using Value = std::string;
+
+    saga_test::property_checker << [](Value const & value, Value const & new_value)
+    {
+        saga::with_old_value<Value> obj(value);
+
+        REQUIRE(obj.value() == value);
+        REQUIRE(obj.old_value() == value);
+
+        obj = new_value;
+
+        REQUIRE(obj.value() == new_value);
+        REQUIRE(obj.old_value() == value);
+
+        obj.revert();
+
+        REQUIRE(obj.value() == value);
+        REQUIRE(obj.old_value() == value);
+    };
+}
