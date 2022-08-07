a823ef46519b8ed4a1397cc1c3be4d535718672f
galushin <galushin@gmail.com>
2022-08-07 10:43:28 +0700

    PE 36: Покрытие тестами дополнительной проверки
    #1065

--- tests/ProjectEuler/projectEuler.cpp
+++ tests/ProjectEuler/projectEuler.cpp
@@ -2087,6 +2087,7 @@ TEST_CASE("PE 036")
 
     REQUIRE(::is_palindrome_in_base(IntType(585), 2));
     REQUIRE(::is_palindrome_in_base(IntType(585), 10));
+    REQUIRE(!::is_palindrome_in_base(IntType(583), 10));
 
     auto result = IntType(0);
 
