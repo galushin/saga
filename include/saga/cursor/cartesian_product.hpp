881c13a330ea7b71da86224ba2d5acee43de4aff
galushin <galushin@gmail.com>
2023-01-04 16:22:27 +0700

    ProjectEuler : задача 61
    * Сама задача
    * Курсор декартового произведения обобщён на произвольное количество аргументов
    
    Close #1157

--- include/saga/cursor/cartesian_product.hpp
+++ include/saga/cursor/cartesian_product.hpp
@@ -28,55 +28,122 @@ SAGA -- это свободной программное обеспечение:
 
 namespace saga
 {
-    template <class InputCursor, class ForwardCursor, class Difference = std::ptrdiff_t>
+    namespace detail
+    {
+        template <class... Types>
+        struct tuple_or_pair
+        {
+            using type = std::tuple<Types...>;
+        };
+
+        template <class T1, class T2>
+        struct tuple_or_pair<T1, T2>
+        {
+            using type = std::pair<T1, T2>;
+        };
+    }
+
+    template <class... Types>
+    using tuple_or_pair = typename detail::tuple_or_pair<Types...>::type;
+
+    template <class InputCursor, class... ForwardCursors>
     class cartesian_product_cursor
-     : saga::cursor_facade<cartesian_product_cursor<InputCursor, ForwardCursor>
-                          , std::pair<cursor_reference_t<InputCursor>
-                                     , cursor_reference_t<ForwardCursor>>>
+     : saga::cursor_facade<cartesian_product_cursor<InputCursor, ForwardCursors...>
+                          , tuple_or_pair<cursor_reference_t<InputCursor>
+                                         ,cursor_reference_t<ForwardCursors>...>>
     {
     public:
         // Типы
-        using reference = std::pair<cursor_reference_t<InputCursor>
-                                   , cursor_reference_t<ForwardCursor>>;
-
-        using difference_type = Difference;
-        using cursor_category = saga::cursor_category_t<InputCursor>;
+        using reference = tuple_or_pair<cursor_reference_t<InputCursor>
+                                       ,cursor_reference_t<ForwardCursors>...>;
+        using difference_type = std::ptrdiff_t;
+        using cursor_category = std::input_iterator_tag;
 
         // Создание
-        explicit cartesian_product_cursor(InputCursor cur1, ForwardCursor cur2)
-         : cur1_(std::move(cur1))
-         , cur2_(std::move(cur2))
+        explicit cartesian_product_cursor(InputCursor cur1, ForwardCursors... others)
+         : curs_(std::move(cur1), std::move(others)...)
         {
-            cur2_.forget_front();
+            this->forget_fronts_impl(std::index_sequence_for<ForwardCursors...>{});
         }
 
         // Курсор ввода
         bool operator!() const
         {
-            return !this->cur1_ || !this->cur2_;
+            return this->is_empty_impl(std::make_index_sequence<sizeof...(ForwardCursors) + 1>{});
         }
 
         reference front() const
         {
-            return reference(this->cur1_.front(), this->cur2_.front());
+            assert(!!*this);
+
+            return this->front_impl(std::make_index_sequence<sizeof...(ForwardCursors) + 1>{});
         }
 
         void drop_front()
         {
             assert(!!*this);
 
-            ++ this->cur2_;
-
-            if(!this->cur2_)
+            if(this->increment_impl(std::integral_constant<std::size_t, 1>{}))
             {
-                ++ this->cur1_;
-                this->cur2_ = this->cur2_.dropped_front();
+                std::get<0>(this->curs_).drop_front();
             }
         }
 
     private:
-        InputCursor cur1_;
-        ForwardCursor cur2_;
+        template <std::size_t... Indices>
+        void forget_fronts_impl(std::index_sequence<Indices...>)
+        {
+            static_assert(sizeof...(Indices) == sizeof...(ForwardCursors));
+
+            (std::get<Indices+1>(this->curs_).forget_front(),...);
+        }
+
+        template <std::size_t... Indices>
+        bool is_empty_impl(std::index_sequence<Indices...>) const
+        {
+            static_assert(sizeof...(Indices) == sizeof...(ForwardCursors) + 1);
+
+            return (!std::get<Indices>(this->curs_) || ...);
+        }
+
+        template <std::size_t... Indices>
+        reference front_impl(std::index_sequence<Indices...>) const
+        {
+            static_assert(sizeof...(Indices) == sizeof...(ForwardCursors) + 1);
+
+            return reference(std::get<Indices>(this->curs_).front()...);
+        }
+
+        template <std::size_t Index>
+        bool increment_impl(std::integral_constant<std::size_t, Index>)
+        {
+            if(!this->increment_impl(std::integral_constant<std::size_t, Index+1>{}))
+            {
+                return false;
+            }
+
+            std::get<Index>(this->curs_).drop_front();
+
+            if(!std::get<Index>(this->curs_))
+            {
+                std::get<Index>(this->curs_)
+                    = std::move(std::get<Index>(this->curs_)).dropped_front();
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        bool increment_impl(std::integral_constant<std::size_t, sizeof...(ForwardCursors) + 1>)
+        {
+            return true;
+        }
+
+    private:
+        std::tuple<InputCursor, ForwardCursors...> curs_;
     };
 
     namespace cursor
